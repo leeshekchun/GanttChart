4993e6487b3f01bbc012c30c1d8d958ac6bfa945
Chris Lee <leeshekchun@gmail.com>
2023-04-04 16:48:47 -0700

    create api route

--- server.js
+++ server.js
@@ -7,6 +7,12 @@ const app = express();
  
 app.use(express.static(path.join(__dirname, "public")));
 app.use(bodyParser.urlencoded({ extended: true }));
+
+// add a dataProcessor route
+app.all(/api/, function(req, res, next){
+    console.log(`\n${req.method} ${req.url} --> ${JSON.stringify(req.body, '\t', 2)}`);
+    res.status(200).end();
+});
  
 app.listen(port, () =>{
     console.log("Server is running on port "+port+"...");
